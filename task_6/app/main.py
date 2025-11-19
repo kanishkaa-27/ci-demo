57957a63c0516c1ba37444e102a7d9633641326d
kanishkaa-27 <kanish.purple@gmail.com>
2025-11-19 14:21:47 +0530

    Update main.py

--- task_6/app/main.py
+++ task_6/app/main.py
@@ -12,7 +12,7 @@ def home():
 
 @app.route("/metrics")
 def metrics():
-    return generate_latest(), 200
+    return generate_latest(), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
