0e84b9b1cdb9bf9fd79c9320dda842968ddbe07e
kanishkaa-27 <kanish.purple@gmail.com>
2025-11-19 11:31:53 +0530

    pushing to a remote

---
+++ task_1/app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"message": "Hello from CI demo!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
